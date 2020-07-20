4ef5c3fc29d876b3da597fda3dbbe092a2193454
lpaulovt <silvavitorlopes711@gmail.com>
2020-07-20 13:17:55 -0300

    chore: API CRUD

--- src/index.js
+++ src/index.js
@@ -1,48 +1,66 @@
 const express = require('express');
+const { uuid } = require('uuidv4')
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json(
-    {
-      message: 'Hello World!',
-      message2: 'Welcome to Node World!'   
-    })
-});
+app.use(express.json());
+
+const projects = [];
 
 app.get('/projects', (request, response) => {
-  return response.json([
-    'GoBarber',
-    'GoRestaurant',
-    'GitHub Explorer'
-    ]);
+  // const {title, owner, size } = request.query;
+
+  // console.log(title);
+  // console.log(owner);
+  // console.log(size);
+
+  return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json([
-    'GoBarber',
-    'GoRestaurant',
-    'GitHub Explorer',
-    'BeTheHero'
-    ]);
+  const {title, owner} = request.body;
+
+  const project = {id: uuid(), title, owner};
+
+  projects.push(project);
+
+  return response.json(project);
 });
 
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'GoBarber -1 ',
-    'GoRestaurant',
-    'GitHub Explorer',
-    'BeTheHero'
-    ]);
+  const {id} = request.params;
+  const {title, owner} = request.body;
+ 
+  const projectIndex = projects.findIndex( project => project.id == id);
+
+  if (projectIndex < 0 ) {
+    return response.status(400).json({ error: "Project not found!"});
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'GoBarber',
-    'GoRestaurant',
-    'GitHub Explorer',
-    ]);
+  const {id} = request.params;
+  
+  const projectIndex = projects.findIndex( project => project.id = id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ message: "Project not found!"})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 
