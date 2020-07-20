0d36d75628721fbabcc123b1b87651ad3b1def9e
lpaulovt <silvavitorlopes711@gmail.com>
2020-07-20 11:05:20 -0300

    chore: Add HTTP methods

--- src/index.js
+++ src/index.js
@@ -10,6 +10,42 @@ app.get('/', (request, response) => {
     })
 });
 
+app.get('/projects', (request, response) => {
+  return response.json([
+    'GoBarber',
+    'GoRestaurant',
+    'GitHub Explorer'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'GoBarber',
+    'GoRestaurant',
+    'GitHub Explorer',
+    'BeTheHero'
+    ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'GoBarber -1 ',
+    'GoRestaurant',
+    'GitHub Explorer',
+    'BeTheHero'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'GoBarber',
+    'GoRestaurant',
+    'GitHub Explorer',
+    ]);
+});
+
+
 app.listen(3333 , () => {
-  console.log('Server running!')
+  console.log('🚀  Back-end started!');
 });
